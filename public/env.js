ed8faa6ede981132f0d3c3f120f4acd9e96185e6
IPTLP0032 <arun_kumar271@hotmail.com>
2021-08-07 23:40:41 +0530

    Connect with mongodb, create and get data from projectCollection and timeSeriesDataCollection.

--- public/env.js
+++ public/env.js
@@ -1,27 +1,80 @@
-const testButtonFunction=()=>{
-  alert('Thank you for clicking')
+const getProjects = () => {
+  $.get('/api/projects', (response) => {
+    if (response.statusCode == 200){
+      console.log(response);
+      addProjects(response.data);
+    }
+    else {
+      console.log(response);
+    }
+  }
+  )
 }
 
-// connect to the socket
+const addProjects = (items) => {
+  items.forEach(item => {
+    let itemToAppend = 
+    '<div class="col l4 s12 m7">'+
+    '<div class="card">' +
+      '<h1 class="card-title center">'+ item.title + '</h1>' +
+      '<div class="card-image">' + 
+        '<img src="'+ item.image + '" style="height: 50%; width: 50%; padding-right: 5px; padding-left: 5px;">' +
+      '</div>' + 
+      '<div class="card-content">' +
+        '<p>' + item.description + '</p>' + 
+      '</div>' + 
+      '<div class="card-action">' + 
+        '<a href="'+ item.link +'"><img src="assets/github.png" style="height: 20px; padding-right: 15px;"></i> Checkout the project !</a>' + 
+      '</div>' + 
+    '</div>' +
+  '</div>' ;
+  $("#projects").append(itemToAppend);
+  });
+}
 
-let socket = io();
+
+// Add sample chart
+// https://canvasjs.com/html5-javascript-line-chart/
+let timeSeriesData;
+
+const getTimeSeriesData = () => {
+  $.get('/api/time-series-data', (response) => {
+    if (response.statusCode == 200){
+      // console.log(response);
+      
+      timeSeriesData = 
+        [{
+          type : response.data[0].type, 
+          indexLabelFontSize : response.data[0].indexLabelFontSize, 
+          dataPoints: response.data[0].dataPoints
+        }];
+
+        console.log(timeSeriesData);
+
+        var chart = new CanvasJS.Chart("chartContainer", {
+          animationEnabled: true,
+          theme: "light1",
+          title:{
+            text: "Time-series data"
+          },
+          data: timeSeriesData
+        });
+        
+        chart.render();
+      
+    }
+    else {
+      console.log(response);
+    }
+  }
+  )
+}
 
 
-socket.on('number', (msg) => {
-    console.log('Random number: ' + msg);
-})
 
-console.log('test')
 $(document).ready(function(){
-  console.log('Ready')
+  // addProjects(projectList);
+  getProjects();
+  getTimeSeriesData();
   
-  //bind the button
-  $('#testButton').click(testButtonFunction)
-
-  //test get call
-  $.get('/test?user_name="Fantastic User"',(result)=>{
-    console.log(result)
-  })
-
-
-})
+});
--- server.js
+++ server.js
@@ -1,40 +1,78 @@
 let express = require("express");
 let app = express();
+const MongoClient = require("mongodb").MongoClient;
 
-//var app = require('express')();
-let http = require('http').createServer(app);
-let io = require('socket.io')(http);
-
-
-
-
+// let http = require('http').createServer(app);
 
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
 
-app.get("/test", function (request, response) {
-  var user_name = request.query.user_name;
-  response.end("Hello " + user_name + "!");
-});
+const uri = "mongodb+srv://arun:arun123@cluster0.epbul.mongodb.net/saifedb?retryWrites=true&w=majority"
+const client = new MongoClient(uri, {useNewUrlParser : true});
+let projectCollection;
+let timeSeriesDataCollection;
+
+const createProjectCollection = (collectionName) => {
+  client.connect((err, db) => {
+    projectCollection = client.db().collection(collectionName);
+    if (!err){
+      console.log("MonogoDB Connected")
+    }
+    else {
+      console.log("DB Error: ", err);
+      process.exit(1);
+    }
+  })
+}
+
+const createTimeSeriesDataCollection = (collectionName) => {
+  client.connect((err, db) => {
+    timeSeriesDataCollection = client.db().collection(collectionName);
+    if (!err){
+      console.log("MonogoDB Connected")
+    }
+    else {
+      console.log("DB Error: ", err);
+      process.exit(1);
+    }
+  })
+}
+
+const getProjects = (callback) => {
+  projectCollection.find({}).toArray(callback);
+}
+
+app.get('/api/projects', (req, res) => {
+  getProjects((err, result) => {
+    if(err) {
+      res.json({statusCode: 400, message: err})
+    }
+    else{
+      res.json({statusCode: 200, message: "Success", data: result})
+    }
+  })
+})
+
+const  getTimeSeriesData = (callback) => {
+  timeSeriesDataCollection.find({}).toArray(callback);
+}
 
 
-//socket test
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-  setInterval(()=>{
-    socket.emit('number', parseInt(Math.random()*10));
-  }, 1000);
+app.get('/api/time-series-data', (req, res) => {
+  getTimeSeriesData((err, result) => {
+    if(err) {
+      res.json({statusCode: 400, message: err})
+    }
+    else{
+      res.json({statusCode: 200, message: "Success", data: result})
+    }
+  })
+})
 
-});
-
-
-http.listen(port,()=>{
+app.listen(port,()=>{
   console.log("Listening on port ", port);
+  createProjectCollection("projects");
+  createTimeSeriesDataCollection("time-series-data");
 });
 
-//this is only needed for Cloud foundry 
-require("cf-deployment-tracker-client").track();
